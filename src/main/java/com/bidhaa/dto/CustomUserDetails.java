54c799dc179cb39bcd710022cd4bdc0c0ec73986
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-26 19:14:30 +0300

    configure roles to CustomUserDetails
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

--- src/main/java/com/bidhaa/dto/CustomUserDetails.java
+++ src/main/java/com/bidhaa/dto/CustomUserDetails.java
@@ -1,11 +1,18 @@
 package com.bidhaa.dto;
 
+import com.bidhaa.model.Privilege;
+import com.bidhaa.model.Role;
 import com.bidhaa.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 @Data
 public class CustomUserDetails implements UserDetails {
@@ -14,34 +21,6 @@ public class CustomUserDetails implements UserDetails {
     public CustomUserDetails(User user) {
         this.user = user;
     }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    /**
-     * Internally, user security requires a list of authorities, roles, that the user has. This method is a simple way to provide those.
-     * Note that SimpleGrantedAuthority requests the format ROLE_role name all in capital letters!
-     *
-     * @return The list of authorities, roles, this user object has
-     */
-//    @JsonIgnore
-//    public List<SimpleGrantedAuthority> getAuthorities()
-//    {
-//        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
-//        Set<UserRoles> roles = user.getRoles();
-//
-//        for (UserRoles r : roles)
-//        {
-//            String myRole = "ROLE_" + r.getRole()
-//                    .getRoleName()
-//                    .toUpperCase();
-//            rtnList.add(new SimpleGrantedAuthority(myRole));
-//        }
-//
-//        return rtnList;
-//    }
 
 
     @Override
@@ -72,5 +51,52 @@ public class CustomUserDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return user.getStatus();
+    }
+
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return getGrantedAuthorities(getPrivileges(user.getRoles()));
+//    }
+
+    /**
+     * Internally, user security requires a list of authorities, roles, that the user has. This method is a simple way to provide those.
+     * Note that SimpleGrantedAuthority requests the format ROLE_role name all in capital letters!
+     *
+     * @return The list of authorities, roles, this user object has
+     */
+    @JsonIgnore
+    public List<SimpleGrantedAuthority> getAuthorities()
+    {
+        List<SimpleGrantedAuthority> rtnList = new ArrayList<>();
+        Collection<Role> roles = user.getRoles();
+
+        for (Role r : roles)
+        {
+            String myRole = r.getName().toUpperCase();
+            rtnList.add(new SimpleGrantedAuthority(myRole));
+        }
+
+        return rtnList;
+    }
+    public List<String> getPrivileges(Collection<Role> roles) {
+
+        List<String> privileges = new ArrayList<>();
+        List<Privilege> collection = new ArrayList<>();
+        for (Role role : roles) {
+            privileges.add(role.getName());
+            collection.addAll(role.getPrivileges());
+        }
+        for (Privilege item : collection) {
+            privileges.add(item.getName());
+        }
+        return privileges;
+    }
+
+    public List<GrantedAuthority> getGrantedAuthorities(List<String> privileges) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String privilege : privileges) {
+            authorities.add(new SimpleGrantedAuthority(privilege));
+        }
+        return authorities;
     }
 }
