53d50fbd3693aed4bb5576853e471d69d2476358
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-26 19:20:58 +0300

    Configure roles to signup and login - Create ability for a user to be created and their roles configured - Add roles of logged-in user to jwt token
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

--- src/main/java/com/bidhaa/service/UserService.java
+++ src/main/java/com/bidhaa/service/UserService.java
@@ -4,7 +4,11 @@ package com.bidhaa.service;
 import com.bidhaa.dto.*;
 import com.bidhaa.mappers.UserMapper;
 import com.bidhaa.mappers.UserMapperUpdate;
+import com.bidhaa.model.Privilege;
+import com.bidhaa.model.Role;
 import com.bidhaa.model.User;
+import com.bidhaa.repository.PrivilegeRepository;
+import com.bidhaa.repository.RoleRepository;
 import com.bidhaa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +17,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
@@ -25,10 +31,8 @@ import org.springframework.validation.FieldError;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.time.Instant;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -37,7 +41,13 @@ import java.util.UUID;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+    private final PrivilegeRepository privilegeRepository;
     private final PasswordEncoder passwordEncoder;
+
+    private final CustomUserDetailsService customUserDetailsService;
 
     private final AuthenticationManager authenticationManager;
     private final JwtEncoder jwtEncoder;
@@ -57,9 +67,22 @@ public class UserService {
                     .body(response);
         }
         try {
-
-            var user = userMapper.toUser(request);
             if (!emailExists(request.email())) {
+            var user = userMapper.toUser(request);
+            Privilege readPrivilege =  createPrivilegeIfNotFound("READ_PRIVILEGE");
+            Privilege writePrivilege = createPrivilegeIfNotFound("WRITE_PRIVILEGE");
+            log.info(request.role());
+            if(request.role().equals("ADMIN")){
+                List<Privilege> adminPrivileges = Arrays.asList(readPrivilege, writePrivilege);
+                createRoleIfNotFound("ROLE_ADMIN", adminPrivileges);
+                Role adminRole = roleRepository.findByName("ROLE_ADMIN");
+                log.warn(String.valueOf(adminRole));
+                user.setRoles(Arrays.asList(adminRole));
+            }else {
+                Role userRole = roleRepository.findByName("ROLE_USER");
+                user.setRoles(Arrays.asList(userRole));
+                createRoleIfNotFound("ROLE_USER", Arrays.asList(readPrivilege));
+            }
                 user.setStatus(true);
                 user.setPassword(passwordEncoder.encode(request.password()));
                 userRepository.save(user);
@@ -97,15 +120,26 @@ public class UserService {
                 response = new LoginResponse("Unable to Login", token.orElse(""),Status.FAILED);
                 return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
             } else {
+                final UserDetails userDetails = customUserDetailsService.loadUserByUsername(request.email());
+                userDetails.getAuthorities().stream().map(s ->
+                                new SimpleGrantedAuthority(s.getAuthority())).
+                        filter(Objects::nonNull).
+                        collect(Collectors.toList());
+                String roles = userDetails.getAuthorities().stream().map(s ->
+                                new SimpleGrantedAuthority(s.getAuthority())).
+                        filter(Objects::nonNull).
+                        collect(Collectors.toList()).toString();
+//                log.warn(roles);
                 var now = Instant.now();
                 var expiry = 36000L;
+
                 var claims =
                         JwtClaimsSet.builder()
-                                .issuer("example.io")
+                                .issuer("bidhaa")
                                 .issuedAt(now)
                                 .expiresAt(now.plusSeconds(expiry))
                                 .subject("something")
-                                .claim("roles", "ROLE_ADMIN")
+                                .claim("roles", roles)
                                 .build();
                 token = this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue().describeConstable();
                 response = new LoginResponse("User logged in successfully", token.orElse(""),Status.SUCCESS);
@@ -116,7 +150,7 @@ public class UserService {
         } catch (Exception ex) {
             StringWriter sw = new StringWriter();
             ex.printStackTrace(new PrintWriter(sw));
-            log.error("Login Error", sw);
+            log.error("Login Error", ex);
             response = new LoginResponse("Login Error", token.orElse(""),Status.FAILED);
             return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
         }
@@ -207,5 +241,28 @@ public class UserService {
         }
     }
 
+    Privilege createPrivilegeIfNotFound(String name) {
 
+        Privilege privilege = privilegeRepository.findByName(name);
+        if (privilege == null) {
+            privilege = new Privilege();
+            privilege.setName(name);
+            privilege.setStatus(true);
+            privilegeRepository.save(privilege);
+        }
+        return privilege;
+    }
+
+
+    void createRoleIfNotFound(String name, Collection<Privilege> privileges) {
+
+        Role role = roleRepository.findByName(name);
+        if (role == null) {
+            role = new Role();
+            role.setName(name);
+            role.setStatus(true);
+            role.setPrivileges(privileges);
+            roleRepository.save(role);
+        }
+    }
 }
