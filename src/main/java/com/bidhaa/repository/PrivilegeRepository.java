aa49046f0dca9e3618da072e3bba1980a03e19f7
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-26 19:18:15 +0300

    Add some methods to privilege and role repositories
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

--- src/main/java/com/bidhaa/repository/PrivilegeRepository.java
+++ src/main/java/com/bidhaa/repository/PrivilegeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PrivilegeRepository extends JpaRepository<Privilege, UUID> {
+    Privilege findByName(String name);
 }
