a29bb73a471971aab391a07b141f982cd1753ded
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-26 19:13:04 +0300

    Intitial application files I forgot to commit lol 🥲
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

---
+++ src/main/java/com/bidhaa/BidhaaApplication.java
@@ -0,0 +1,13 @@
+package com.bidhaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidhaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidhaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bidhaa/BidhaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidhaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidhaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
