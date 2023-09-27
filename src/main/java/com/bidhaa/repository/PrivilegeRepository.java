a21377f848a3cf72f7a6ee21a1ec883890837aa8
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-09-27 15:31:52 +0300

    Add repository annotations
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

--- src/main/java/com/bidhaa/repository/PrivilegeRepository.java
+++ src/main/java/com/bidhaa/repository/PrivilegeRepository.java
@@ -2,9 +2,11 @@ package com.bidhaa.repository;
 
 import com.bidhaa.model.Privilege;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PrivilegeRepository extends JpaRepository<Privilege, UUID> {
     Privilege findByName(String name);
 }
--- src/main/java/com/bidhaa/repository/RoleRepository.java
+++ src/main/java/com/bidhaa/repository/RoleRepository.java
@@ -3,9 +3,11 @@ package com.bidhaa.repository;
 import com.bidhaa.model.Role;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, UUID> {
     Role findByName(String name);
 
