b58f3fdb2b62f50d43051cf8fa867dd942a6cf5d
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-10-06 11:54:47 +0300

    update controllerAdvice to display exceptions
    Signed-off-by: Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>

--- src/main/java/com/bidhaa/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/bidhaa/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.bidhaa.exceptions;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,17 +12,16 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 
 @ControllerAdvice
-@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
+    public ResponseEntity<?> handleGenericException(Exception ex) {
         String errorMessage = "An unexpected error occurred.";
         StringWriter sw = new StringWriter();
         ex.printStackTrace(new PrintWriter(sw));
-        log.error(errorMessage, sw);
+//        System.out.println(errorMessage + sw);
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorMessage);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(sw);
     }
 }
 
